a259d81ae07225118229c9706ac1e4360f11a689
AntonioDelgado <antonio.delgado.peris@cern.ch>
2014-06-06 15:43:23 +0200

    Include rst file date and links to rst and twiki versions in every html page

--- cgi-bin/txt2html/command.sh
+++ cgi-bin/txt2html/command.sh
@@ -5,8 +5,15 @@ BASEDIR=/var/www/cgi-bin/howtos/txt2html/
 fname="$1"
 page="$2"
 
+filedate=`date -u '+%Y-%m-%d %H:%M UTC' -r $fname`
 
-html1="<center> \n <a href=\"/cgi-bin/howtos/howtos.py\">Back Home</a>  \&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp; \n  <a href=\"/cgi-bin/howtos/howtos.py?page=${page}\&action=edit\">Edit</a> \n</center>\n"
+html1="<center>"
+html1="$html1 \n <a href=\"/cgi-bin/howtos/howtos.py\">Back Home</a>  \&nbsp;\&nbsp;\&nbsp;\&nbsp;\&nbsp;"
+html1="$html1 \n <a href=\"/cgi-bin/howtos/howtos.py?page=${page}\&format=txt\">txt</a> \&nbsp;\&nbsp;\&nbsp;"
+html1="$html1 \n <a href=\"/cgi-bin/howtos/howtos.py?page=${page}\&format=twiki\">twiki</a> \&nbsp;\&nbsp;\&nbsp;\&nbsp;"
+html1="$html1 \n <a href=\"/cgi-bin/howtos/howtos.py?page=${page}\&action=edit\">Edit</a>"
+html1="$html1 \n</center>\n"
 
 rst2html -t --stylesheet ${BASEDIR}/mystyle.css,${BASEDIR}/pygment.css \
-   $fname | sed "/^<h1 class=\"title/s#^#$html1#"   
+   $fname | sed -e "/^<h1 class=\"title/s#^#$html1#" -e "s#Generated.*#Last update: \&nbsp;\&nbsp; $filedate. <br>\n&#"
+
